19bf800cb5aebc8877b51d1e32183469cb865760
F_picacho <2314008827@qq.com>
2023-05-26 15:01:15 +0800

    ✨ feat(布局): 新增布局文件

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="footer">
+    <p>footer</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 默认布局 -->
+  <div>
+    <p>login</p>
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
